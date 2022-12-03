bfc454fe822ed73af99f34f458146321162973f8
FarBusC4 <marcosfer243@gmail.com>
2022-12-03 12:16:25 -0300

    Fix google auth problem

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const debug = require("debug")("app:server");
-
+const path = require("path");
+const { OAuth2Client } = require("google-auth-library");
 const { Config } = require("./src/config/index.js");
 const { ProductsAPI } = require("./src/products/index");
 const { UsersAPI } = require("./src/users/index");
@@ -8,6 +9,8 @@ const { IndexApi, NotFoundApi } = require("./src/index/index.js");
 const cors = require("cors");
 
 const app = express();
+dotenv.config();
+const client = new OAuth2Client(process.env.REACT_APP_GOOGLE_CLIENT_ID);
 
 app.use(express.json());
 app.use(cors());
@@ -16,6 +19,31 @@ IndexApi(app);
 ProductsAPI(app);
 UsersAPI(app);
 NotFoundApi(app);
+
+const users = [];
+
+function upsert(array, item) {
+  const i = array.findIndex((_item) => _item.email === item.email);
+
+  if (i > -1) array[i] = item;
+  else array.push(item);
+}
+
+app.post("/api/google-login", async (req, res) => {
+  const { token } = req.body;
+
+  const ticket = await client.verifyIdToken({
+    idToken: token,
+    audience: process.env.CLIENT_ID,
+  });
+
+  const { name, email, picture } = ticket.getPayload();
+
+  upsert(users, { name, email, picture });
+  res.status(201);
+
+  res.json({ name, email, picture });
+});
 
 // Modules
 
